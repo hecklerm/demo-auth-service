0fcc916d641e25fe1ff9f1d0b598431bd953f83b
Mark Heckler <mark.heckler@gmail.com>
2016-02-17 20:45:57 -0600

    Initial commit.

---
+++ src/main/java/org/thehecklers/AuthServiceApplication.java
@@ -0,0 +1,16 @@
+package org.thehecklers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+@SpringBootApplication
+@EnableAuthorizationServer
+@EnableDiscoveryClient
+public class AuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServiceApplication.class, args);
+	}
+}
